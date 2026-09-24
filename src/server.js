1111af2f34f14bcbecaa36703189ca7def95c846
a.viazovskyi <sentynel2014@gmail.com>
2026-09-24 17:33:27 +0200

    Remove Docker setup and merge app into server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,31 @@
 import 'dotenv/config';
 
-import { app } from './app.js';
+import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+
+import { errorHandler } from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
 
 const PORT = process.env.PORT ?? 3000;
+
+const app = express();
+
+app.use(
+  express.json({
+    type: ['application/json', 'application/vnd.api+json'],
+    limit: '100kb',
+  }),
+);
+app.use(logger);
+app.use(cors());
+app.use(cookieParser());
+
+app.use(notFoundHandler);
+app.use(errors());
+app.use(errorHandler);
 
 export const startServer = () =>
   app.listen(PORT, () => {
@@ -12,3 +35,5 @@ export const startServer = () =>
 if (process.env.NODE_ENV !== 'test') {
   startServer();
 }
+
+export { app };
