6e58b8f3803b63db080e06974d1f4a17e1f02901
IrinaV1 <vyustirina@gmail.com>
2026-09-25 22:24:53 +0300

    add sort by rate

--- src/controllers/locationsController.js
+++ src/controllers/locationsController.js
@@ -1,11 +1,11 @@
 import { Location } from "../models/location.js";
 
 export const getAllLocations = async (req, res) => {
-const {page = 1, limit = 10, region, type,  search} = req.query;
+const {page = 1, limit = 10, region, type, search, rate, sortBy = 'rate',  sortOrder = 'desc'} = req.query;
 
 const skip = (page - 1) * limit;
 
-const locationsQuery = Location.find({});
+const locationsQuery = Location.find();
 
 if(region){
   locationsQuery.where('region').equals(region);
@@ -24,9 +24,15 @@ locationsQuery.where({
 })
 };
 
+  if (rate) {
+    locationsQuery.where('rate').gte(Number(rate));
+  }
+
+const sortDirection = sortOrder === 'asc' ? 1 : -1;
+
 const [totalLocations, locations] = await Promise.all([
-  locationsQuery.find().clone().countDocuments(),
-  locationsQuery.find().skip(skip).limit(limit),
+  locationsQuery.clone().countDocuments(),
+  locationsQuery.clone().sort({[sortBy]: sortDirection}).skip(skip).limit(limit),
 ]);
 
 const totalPages = Math.ceil(totalLocations / limit);
--- src/validations/locationsValidation.js
+++ src/validations/locationsValidation.js
@@ -7,6 +7,9 @@ limit: Joi.number().integer().min(1).max(50).default(10),
 region: Joi.string().trim(),
 type: Joi.string().trim(),
 search: Joi.string().trim().allow(''),
+rate: Joi.number().min(1).max(5),
+sortBy: Joi.string().valid('rate').default('rate'),
+sortOrder: Joi.string().valid('asc', 'desc').default('desc'),
   })
 };
 
