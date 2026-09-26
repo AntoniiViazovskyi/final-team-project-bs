57877c058703ad234a5218702e9720792333377a
a.viazovskyi <sentynel2014@gmail.com>
2026-09-26 21:22:09 +0200

    No business logic changes. Parallel development caused naming and routing conflicts across several files. These conflicts have been resolved.

--- src/routes/locationRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getLocationById } from '../controllers/locationController.js';
-
-const locationRoutes = Router();
-
-locationRoutes.get('/:locationId', getLocationById);
-
-export default locationRoutes;
--- src/routes/locationsRoutes.js
+++ src/routes/locationsRoutes.js
@@ -1,10 +1,13 @@
 import { celebrate } from 'celebrate';
 import { Router } from 'express';
-import { getAllLocationsSchema } from '../validations/locationsValidation.js';
+
+import { getLocationById } from '../controllers/locationController.js';
 import { getAllLocations } from '../controllers/locationsController.js';
+import { getAllLocationsSchema } from '../validations/locationsValidation.js';
 
 const locationsRoutes = Router();
 
 locationsRoutes.get('/', celebrate(getAllLocationsSchema), getAllLocations);
+locationsRoutes.get('/:locationId', getLocationById);
 
 export default locationsRoutes;
