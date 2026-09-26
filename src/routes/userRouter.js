a1402fdfbec2af3a0c09d799127189c34808a519
Oleh <babijchik@gmail.com>
2026-09-26 19:35:41 +0300

    update auth middleware

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { getUserProfile } from '../controllers/userController.js';
-import { authenticate } from '../middleware/authMiddleware.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const userRouter = Router();
 
